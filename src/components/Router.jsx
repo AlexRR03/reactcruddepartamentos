85f19f441fd2013bec4a6fa012ea88f10e7e1119
Alex <alexciempo03@gmail.com>
2024-10-25 08:18:11 +0200

    crud actualizar departamentos

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -4,13 +4,18 @@ import HomeDepartamentos from './HomeDepartamentos'
 import MenuDepartamentos from './MenuDepartamentos'
 import CreateDepartamentos from './CreateDepartamentos'
 import DetalleDepartamento from './DetalleDepartamento'
-
+import UpdateDepartamento from './UpdateDepartamentos'
 export default class Router extends Component {
   render() {
     function DetalleDepartamentoElement (){
         let {iddepartamento} = useParams();
         return(<DetalleDepartamento id={iddepartamento}/>)
     }
+    function UpdateDepartamentoElement() {
+      let { id, nombre, localidad } = useParams();
+      return (<UpdateDepartamento id={id} nombre={nombre} localidad={localidad}/>)
+  }
+
     return (
       <div>
         <BrowserRouter>
@@ -19,6 +24,8 @@ export default class Router extends Component {
                 <Route path='/' element={<HomeDepartamentos/>}></Route>
                 <Route path='/create' element = {<CreateDepartamentos />}></Route>
                 <Route path='/detalle/:iddepartamento' element={<DetalleDepartamentoElement/>}></Route>
+                <Route path="/update/:id/:nombre/:localidad" 
+            element={<UpdateDepartamentoElement/>}/>
             </Routes>
         </BrowserRouter>
       </div>
